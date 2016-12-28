81f29b941752aa1657c14a9f436e1db927745d58
raghudevops <raghu.poguri2gmail.com>
2016-12-28 10:59:58 +0530

    trigger from client mechine

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ var app = express();
  
 app.get('/', function (req, res) {
   res.send('Hello Jenkins this is rsync command master ');
+  res.send('Hello Jenkins this is rsync command client ');
 });
  
 app.listen(process.env.PORT || 5000);
